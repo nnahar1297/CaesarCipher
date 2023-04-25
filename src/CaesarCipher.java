ad2d25bf103748eee326a84d891d3355312f1187
Nazmun <nnahar@ggc.edu>
2023-04-25 15:06:18 -0400

    Adding CaesarCipher code

--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class CaesarCipher {
+import static java.lang.Character.isLetter;
+
+public class CaesarCipher {
+    int shift;
+
+    String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    CaesarCipher(int shift){
+        this.shift = shift;
+    }
+
+    public String encrypt(String msg){
+        String encryptedMsg = "";
+
+        for(int i=0; i<msg.length(); i++){
+            int pos = alphabet.indexOf(msg.charAt(i));
+            if(isLetter(msg.charAt(i))){
+                int encryptPos = ((pos + shift) % 26) ;
+                if(encryptPos > 25)
+                    encryptPos = encryptPos - 26;
+                encryptedMsg = encryptedMsg + alphabet.charAt(encryptPos);
+            }
+            else
+                encryptedMsg = encryptedMsg + msg.charAt(i);
+        }
+        return encryptedMsg;
+    }
+
+    public String decrypt(String msg){
+        String decryptedMsg = "";
+
+        for (int i = 0; i < msg.length(); i++) {
+            int pos = alphabet.indexOf(msg.charAt(i));
+            if (isLetter(msg.charAt(i))) {
+                int decryptPos = ((pos - shift) % 26);
+                if (decryptPos < 0)
+                    decryptPos = decryptPos + 26;
+                decryptedMsg = decryptedMsg + alphabet.charAt(decryptPos);
+            } else
+                decryptedMsg = decryptedMsg + msg.charAt(i);
+        }
+        return decryptedMsg;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner prompt = new Scanner(System.in);
+
+        System.out.println("Enter a message to encrypt/ decrypt:");
+        String msg = prompt.nextLine().toUpperCase();
+
+        System.out.println("Enter a shift key for encryption/ decryption: ");
+        int key = prompt.nextInt();
+
+        CaesarCipher ob = new CaesarCipher(key);
+
+        System.out.println("Enter E to encrypt or D to decrypt: ");
+        char c = prompt.next().charAt(0);
+        if(c == 'E' || c == 'e')
+            System.out.println("The encrypted message is:\n" + ob.encrypt(msg));
+        else if(c == 'D' || c == 'd')
+            System.out.println("The decrypted message is:\n" + ob.decrypt(msg));
+        else
+            System.out.println("Wrong input!");
     }
 }
